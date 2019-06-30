115cb415bf27bf882a03ad241c3216825e31d618
kondo takeshi <chaspy@users.noreply.github.com>
2019-07-01 01:39:59 +0900

    Update item

--- bookshelf.rb
+++ bookshelf.rb
@@ -17,6 +17,28 @@ get '/additem' do
 end
 
 post '/update_action' do
-  settings.items.insert_one params
+  if params.has_key?('id')
+    item = params
+    id = BSON::ObjectId(params['id'])
+    item.delete('id')
+    settings.items.update_one({'_id' => id}, item)
+  else
+    settings.items.insert_one params
+  end
   redirect "/", 303
+end
+
+get '/edititem' do
+  p params
+  unless params.has_key?('id')
+    redirect "/", 303
+  end
+
+  obj_id = BSON::ObjectId(params['id'])
+  item_found = settings.items.find({'_id' => obj_id}, :limit => 1)
+  if item_found.count == 0
+    redirect "/", 303
+  else
+    erb :edititem, :locals => { :pagetitle => ':Edit book', :item => item_found.to_a[0] }
+  end
 end
