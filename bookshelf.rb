96251f5406dd8ba783dabaedea64741d275273e3
kondo takeshi <chaspy@users.noreply.github.com>
2019-07-01 01:19:23 +0900

    Insert book

--- bookshelf.rb
+++ bookshelf.rb
@@ -1,5 +1,21 @@
+# confg: utf-8
 require 'sinatra'
+require 'mongo'
+
+configure do
+  client = Mongo::Client.new([ 'mongo0:27017' ], :database => 'bookshelf')
+  set :items, client[:items]
+end
 
 get '/' do
   erb :index, :locals => { :pagetitle => '' }
+end
+
+get '/additem' do
+  erb :additem, :locals => { :pagetitle => ': Add book' }
+end
+
+post '/update_action' do
+  settings.items.insert_one params
+  redirect "/", 303
 end
